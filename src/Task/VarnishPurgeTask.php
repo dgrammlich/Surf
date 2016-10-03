8ea3440c2b5ef79ee6131c6f5beb69d354987f5f
Daniela Grammlich <grammlich@punkt.de>
2016-10-03 11:28:24 +0200

    [TASK] Add documentation in class annotation for `VarnishPurgeTask` (#33)

--- src/Task/VarnishPurgeTask.php
+++ src/Task/VarnishPurgeTask.php
@@ -15,9 +15,18 @@ use TYPO3\Surf\Domain\Model\Node;
  *
  * It takes the following options:
  *
- * * secretFile - path to the secret file, defaults to "/etc/varnish/secret"
- * * purgeUrl - URL (pattern) to purge, defaults to "."
- * * varnishadm - path to the varnishadm utility, defaults to "/usr/bin/varnishadm"
+ * * secretFile (optional) - Path to the secret file, defaults to "/etc/varnish/secret".
+ * * purgeUrl (optional) - URL (pattern) to purge, defaults to ".".
+ * * varnishadm (optional) - Path to the varnishadm utility, defaults to "/usr/bin/varnishadm".
+ *
+ * Example:
+ *  $workflow
+ *      ->setTaskOptions('TYPO3\Surf\Task\VarnishPurgeTask', [
+ *              'secretFile' => '/etc/varnish/secret',
+ *              'purgeUrl' => '.',
+ *              'varnishadm' => '/usr/bin/varnishadm'
+ *          ]
+ *      );
  */
 class VarnishPurgeTask extends \TYPO3\Surf\Domain\Model\Task implements \TYPO3\Surf\Domain\Service\ShellCommandServiceAwareInterface
 {
